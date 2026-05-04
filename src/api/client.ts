14279467fc9be1741442cf9e128859fcc7d86f28
hillsms <hillsmssuport@gmail.com>
2026-05-04 16:10:21 +0400

    api url changed to live

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  // baseURL: '/api',
+  baseURL: 'https://kktcsms.net/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
